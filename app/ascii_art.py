6048b6c12aa2fe99ca02e17c86317c977a10b79f
JustinHBird <justin@thewordisbird.dev>
2019-11-03 11:10:55 -0800

    convert image to 2-d pixel matrix

--- app/ascii_art.py
+++ app/ascii_art.py
@@ -13,6 +13,26 @@ def load_image(jpg_file):
 def image_info(image):
     print(f'Image size: {image.size[0]} x {image.size[1]}')
 
+# 2. Load images pixel data into a 2-d array
+def image_to_pixels(image):
+    pixel_matrix = []
+    pixel_matrix_width = image.size[0]
+    pixel_matrix_height = image.size[1]
+    
+    pixels = image.load()
+    for i in range(pixel_matrix_width):
+        pixel_row = []
+        for j in range(pixel_matrix_height):
+            pixel_row.append(pixels[i, j])
+        pixel_matrix.append(pixel_row)
+    
+    return pixel_matrix
+
+def pixel_matrix(image):
+    print(list(image.getdata()))
+            
+
+
 if __name__ == "__main__":
     # Get relative path to data folder for image file
     app_path = os.path.abspath(__file__)
@@ -24,4 +44,6 @@ if __name__ == "__main__":
 
     image = load_image(jpg_image)
     image_info(image)
+    #image_to_pixels(image)
+    pixel_matrix(image)
     
