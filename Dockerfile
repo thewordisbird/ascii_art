d6df2153fcdcc4883d7fe896cd765789f93a7345
Justin Bird <justin@thewordisbird.dev>
2020-01-17 14:37:37 -0800

    Inital setup for docker development workflow

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.6
+
+# set a directory for the app:
+WORKDIR /usr/src/app
+
+# install uodates and system dependencies:
+RUN apt-get -yqq update
+
+# copy all files to the container
+COPY . .
+
+# Install the app requirements and the app as an editable package (This is for testing only)
+RUN pip install -r requirements.txt && pip install -e .
+
+CMD python ascii_art.py
+
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ascii-art-theW0rdisbird", # Replace with your own username
+    name="ascii-art-theW0rdisbird", 
     version="0.0.1",
     author="Justin Bird",
     author_email="justin.h.bird@gmail.com",
-    description="Convert .jpg's to ascii art.",
+    description="Convert .jpg's or .png's to ascii art.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JustinHBird/microblog",
